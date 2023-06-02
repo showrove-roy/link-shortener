ab26d8d6ee90ca086b16609e6debe5b378ee011a
Showrove Roy <showrove200@gmail.com>
2023-06-03 02:27:34 +0600

    link added ang get done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+
+  const [originalLink, setOriginalLink] = useState(null);
+
+  const linkCollect =(e)=>{
+    e.preventDefault();
+    setOriginalLink(e.target.orginalLink.value);
+
+  }
   return (
     <section className='max-w-7xl mx-auto px-10 '>
       <div className='h-screen flex items-center justify-center'>
-        <div className="grid grid-cols-1 gap-10">
-        <div className='form-control w-full max-w-md'>
-          <label className='label'>
-            <span className='label-text'>Product link</span>
-          </label>
-          <input
-            type='text'
-            placeholder='Type here'
-            className='input input-bordered input-accent w-full max-w-md'
-          />
-        </div>
-        <div className='form-control w-full max-w-md'>
-          <label className='label'>
-            <span className='label-text'>Product link</span>
-          </label>
-          <input
-            type='text'
-            placeholder='Type here'
-            className='input input-bordered input-accent w-full max-w-md'
-          />
-        </div>
-        <button className="btn btn-primary">Button</button>
-        </div>
+        <form onSubmit={linkCollect} className='grid grid-cols-1 gap-10 max-w-lg border-primary'>
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text'>Product link</span>
+            </label>
+            <input
+              type='url'
+              placeholder='Type here'
+              name="orginalLink"
+              className='input input-bordered input-accent w-full'
+              required
+            />
+          </div>
+          <input className='btn btn-primary' type="submit" value="Add" />
+          
+        </form>
       </div>
     </section>
   );
